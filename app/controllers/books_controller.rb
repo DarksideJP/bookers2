41c92ef811338d44e4f9fa233c60d7b2d4ce8aa2
DarksideJP <darknebula1419@gmail.com>
2020-02-10 07:38:54 +0000

    [Fix] 投稿とuserをアソシエーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,29 @@ class BooksController < ApplicationController
   def show
   end
 
+  def new
+  end
+
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "successfully"
+    else
+      render "users/show", notice: "error"
+    end
+  end
+
   def edit
   end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title,:body, :user_id)
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     @book = Book.new
+    @books = @user.books
   end
 
   def edit
@@ -14,21 +15,13 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create
-    @book = Book.new(book_params)
-    @books = Book.all
-    if @book.save
-      redirect_to book_path(@book.id), notice: "successfully"
-    else
-      render :show, notice: "error"
-    end
-  end
-
   def update
   	@user = User.find(params[:id])
   	@user.update(user_params)
   	redirect_to user_path(@user.id)
   end
+
+
   def user_params
   	params.require(:user).permit(:name, :profile_image, :introduction)
 end
