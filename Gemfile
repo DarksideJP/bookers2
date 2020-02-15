74ba8b1a94cd60aed44a9e3d73847912c83bdfd9
DarksideJP <darknebula1419@gmail.com>
2020-02-15 10:30:04 +0000

    [Add] deploy

--- Gemfile
+++ Gemfile
@@ -65,3 +65,9 @@ gem "refile", require: "refile/rails", github: "manfe/refile"
 gem "refile-mini_magick"
 gem "jquery-rails"
 gem "bootstrap-sass","~>3.3.6"
+gem "jquery-rails"
+
+gem 'dotenv-rails'
+group :production do
+  gem 'mysql2'
+end
--- config/puma.rb
+++ config/puma.rb
@@ -35,3 +35,31 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+
+rails_root = Dir.pwd
+
+# 本番環境のみデーモン起動
+
+if Rails.env.production?
+
+	pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+
+	state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+
+	stdout_redirect(
+
+		file.join(rails_root, 'rog', 'puma.log'),
+
+		file.join(rails_root, 'log', 'puma-error.log'),
+
+		true
+
+		)
+
+	#　デーモン
+
+	deamonize
+
+end
